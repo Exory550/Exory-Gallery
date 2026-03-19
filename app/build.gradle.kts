3919f54266cd9e4f89789f90c3a45c1f2efd1e34
Exory550 <exory.official55@gmail.com>
2026-03-19 18:58:48 +0700

    fix: opt-in ExperimentalMaterial3Api global

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
+    }
 
     buildFeatures { compose = true }
 
