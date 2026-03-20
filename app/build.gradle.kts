e322d0580ecab6b42f6042c3f3315fbdf5ad813f
Exory550 <exory.official55@gmail.com>
2026-03-20 10:05:17 +0700

    fix: OptIn ExperimentalFoundationApi ExperimentalAnimationApi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,9 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-        freeCompilerArgs += listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
+        freeCompilerArgs += listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+                "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+                "-opt-in=androidx.compose.animation.ExperimentalAnimationApi"")
     }
 
     buildFeatures { compose = true }
