b0783e3f91537f95a39ac508242e762148a01b8b
Exory550 <exory.official55@gmail.com>
2026-03-20 19:22:55 +0700

    fix: import ExperimentalFoundationApi dan combinedClickable AlbumsScreen

--- app/src/main/java/com/exory550/exorygallery/presentation/screens/albums/AlbumsScreen.kt
+++ app/src/main/java/com/exory550/exorygallery/presentation/screens/albums/AlbumsScreen.kt
@@ -1,5 +1,8 @@
 package com.exory550.exorygallery.presentation.screens.albums
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
